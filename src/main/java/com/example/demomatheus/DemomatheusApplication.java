c07a36492484aea60559918e19802d56e58846d5
mathebas <99615992+mathebas@users.noreply.github.com>
2022-02-13 16:23:34 -0400

    Add files via upload
    Configuração Inicial.

---
+++ src/main/java/com/example/demomatheus/DemomatheusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomatheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomatheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomatheusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demomatheus/DemomatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomatheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomatheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
